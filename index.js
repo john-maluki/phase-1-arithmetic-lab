0833ef453d86eefd1c17632298468c193f999c3f
john <johnmaluki58@gmail.com>
2023-06-13 09:03:01 +0300

    Completed assignment on arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 14;
+const num4 = 10;
+
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10 + 1);
+
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 20, 5);
